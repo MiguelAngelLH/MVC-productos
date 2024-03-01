20c7788759972d6baf56e1234a1fcf255383157b
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2024-03-01 12:58:24 -0600

    Update app.py
    ya esta bien todo

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import web
 
-# Rutas de los controladores
 urls = (
-    '/', 'mvc.controllers.lista_productos.Lista',
-    '/insertar', 'mvc.controllers.insertar_productos.Insertar',
-    '/detalle/(\d+)', 'mvc.controllers.detalle_productos.Detalle',
-     r'/borrar_producto/(\d+)', 'mvc.controllers.borrar_productos.BorrarProducto',
+    '/', 'mvc.controllers.lista_productos.MostrarListaProductos',
+    '/insertar', 'mvc.controllers.insertar_productos.InsertarProducto',
+    r'/detalle_producto/(\d+)', 'mvc.controllers.detalle_productos.DetalleProducto',
+    r'/borrar_producto/(\d+)', 'mvc.controllers.borrar_productos.BorrarProducto',
     r'/actualizar_producto/(\d+)', 'mvc.controllers.actualizar_productos.ActualizarProducto'
-)  # Actualizamos la URL para que coincida con el formulario en el HTML
+)
 
-
-app = web.application(urls, globals())
-
-# Punto de entrada
 if __name__ == "__main__":
-    web.config.debug = False  # Configuración para desactivar el modo debug
+    app = web.application(urls, globals())
     app.run()
